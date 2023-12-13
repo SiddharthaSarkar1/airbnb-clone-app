3a95c95d290659e418590e5be878c0373a7e8430
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2023-12-14 02:47:59 +0530

    listing creation - 1  category selection

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -55,6 +55,15 @@ const LoginModal = () => {
     })
   };
 
+  const toggle = useCallback(
+    () => {
+      loginModal.onClose();
+      registerModal.onOpen();
+    },
+    [loginModal, registerModal],
+  )
+  
+
   const bodyContent = (
     <div className="flex flex-col gap-4">
       <Heading title="Welcome back" subtitle="Login to your account!" />
@@ -95,12 +104,12 @@ const LoginModal = () => {
       />
       <div className="text-neutral-500 test-center mt-4 font-light">
         <div className="flex flex-row justify-center items-center gap-2">
-          <div>Already have an account?</div>
+          <div>First time using airbnb?</div>
           <div
-            onClick={registerModal.onClose}
+            onClick={toggle}
             className="text-neutral-800 cursor-pointer hover:underline"
           >
-            Log in
+            Create an account
           </div>
         </div>
       </div>
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -6,6 +6,7 @@ import { useState, useCallback } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
 import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
@@ -16,6 +17,7 @@ import { signIn } from "next-auth/react";
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
   const [isLoading, setIsLoading] = useState(false);
+  const loginModal = useLoginModal();
 
   const {
     register,
@@ -45,6 +47,14 @@ const RegisterModal = () => {
         setIsLoading(false);
       });
   };
+
+  const toggle = useCallback(
+    () => {
+      registerModal.onClose();
+      loginModal.onOpen();
+    },
+    [loginModal, registerModal],
+  )
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
@@ -96,7 +106,7 @@ const RegisterModal = () => {
         <div className="flex flex-row justify-center items-center gap-2">
           <div>Already have an account?</div>
           <div
-            onClick={registerModal.onClose}
+            onClick={toggle}
             className="text-neutral-800 cursor-pointer hover:underline"
           >
             Log in
